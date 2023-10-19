8b82776e941e650f90ace27448fc1c7d9853bd3e
2559055 <mohamed.hussein@student.moringaschool.com>
2023-10-19 02:39:09 -0700

    fetch lab

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+
+
+async function fetchBooks() {
+  try {
+    const response = await fetch('https://anapioficeandfire.com/api/books', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await response.json();
+    renderBooks(data); 
+
+  } catch (error) {
+    console.error('Error fetching books:', error);
+  }
 }
+
+// import { fetchBooks } frondex';m './i
+ 
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve([{ title: 'Book 1' }, { title: 'Book 2' }]),
+    ok: true,
+  })
+);
+
+describe('fetchBooks', () => {
+  it('sends a fetch request to the API and renders books', async () => {
+    const renderBooks = jest.fn();
+
+  
+
+    await fetchBooks(renderBooks);
+
+    expect(fetch).toHaveBeenCalledWith('https://anapioficeandfire.com/api/books', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      },
+    });
+
+    expect(renderBooks).toHaveBeenCalledWith([{ title: 'Book 1' }, { title: 'Book 2' }]);
+  });
+});
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
